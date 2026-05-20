3005e9f34ca4fdfb277cbb5ec278fc9ca8465f10
Yashvanth-2272 <yashvanthmbme2019@citchennai.net>
2026-05-20 23:45:57 +0800

    Initial Spring Boot project setup

---
+++ src/main/java/com/yash/bank/banking_transfer_system/BankingTransferSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yash.bank.banking_transfer_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransferSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingTransferSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/bank/banking_transfer_system/BankingTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.bank.banking_transfer_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
